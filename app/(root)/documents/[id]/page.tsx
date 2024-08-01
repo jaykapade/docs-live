09d8532187c091b55bc90e6a3612950416e24dc8
jaykapade <jazzdestructor@gmail.com>
2024-08-01 23:26:45 +0530

    Added assets and editor setup

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Editor } from "@/components/editor/Editor";
+import React from "react";
+
+const Document = () => {
+  return (
+    <>
+      <Editor />
+    </>
+  );
+};
+
+export default Document;
